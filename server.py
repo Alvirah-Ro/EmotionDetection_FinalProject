d266ee693879bbdd45c2e510a12048047c750287
Alvirah-Ro <Bajoraneh@hotmail.com>
2024-09-22 22:58:53 -0400

    Added code to server.py and successfully ran application

--- server.py
+++ server.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template, request
-from EmotionDetection.emotion-detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask(__name__)
+app = Flask("Emotion Detector")
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
 
 @app.route("/emotionDetector")
 def RunSentimentAnalysis():
     text_to_analyze = request.args.get('textToAnalyze')
-    response = emotion_detector(text_to_analyze)
-    return "For the given statement, the system response is \'anger\':", anger, "\'disgust\':", disgust, "\'fear\':", fear, "\'joy\':", joy, "and \'sadness\':", sadness, ".  The dominant emotion is", dominant_emotion, "." 
 
-if __name__ = "__main__":
-    app.run(localhost:5000)
+    response = emotion_detector(text_to_analyze)
+
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+    
+    return (
+        f"For the given statement, the system response is: <br> 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, <br>'joy: {joy}, and 'sadness': {sadness}."
+        f"<br>The dominant emotion is <b>{dominant_emotion}</b>." 
+    )
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
